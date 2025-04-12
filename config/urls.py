438060e73041ea3fcd971db9e9777e9875e2f566
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-04-12 16:53:06 +1000

    configure routing

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('students/', include('students.urls')),
+    path('', include('catalog', namespace='catalog')),
 ]
